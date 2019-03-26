0d31a33e3c7e97f453e6c1b9d874292bdb1fc104
Alexander Song <alex.ky.song@gmail.com>
2019-03-26 12:06:59 -0700

    tweaks to UI

--- app/src/main/java/work/koumakan/locationclient/MainActivity.java
+++ app/src/main/java/work/koumakan/locationclient/MainActivity.java
@@ -48,7 +48,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 }
